94725fe879af4f31e8e2cd01da1999a5660c5a8b
Andrei Balan <abalan@orange-vallee.net>
2014-05-05 01:31:06 +0200

    Added license header to all files.

--- src/ro/andreibalan/media/Audio.java
+++ src/ro/andreibalan/media/Audio.java
@@ -1,3 +1,22 @@
+/**
+ * Android Media Library, an media library for the android platform.
+ * 
+ * Copyright (C) 2014 Andrei Balan
+ * 
+ * This file is part of Android Media Library
+ * 
+ * Android Media Library is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * Android Media Libraryis distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with Android Media Library. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Authors: Andrei Balan
+ */
 package ro.andreibalan.media;
 
 public abstract class Audio implements IAudio {
